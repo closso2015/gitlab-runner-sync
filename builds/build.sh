c439989a19eadb115e03ae38c971c1064f6555b5
build <build@bit9.local>
2019-05-03 19:40:11 +0000

    Update build.sh

--- builds/build.sh
+++ builds/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Running the builds"
+echo "Running the builds and will write $1 when complete"
 CNT=0 
 STOPCNT=600
 SLEEP_SEC=1
@@ -11,3 +11,5 @@ while [ $CNT -lt $STOPCNT ]; do
     let "CNT = ${CNT} + 1"
     sleep $SLEEP_SEC
 done
+
+touch $1
