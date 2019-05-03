efc393938d54d3c355d6601c4d646661612438e6
build <build@bit9.local>
2019-05-03 19:40:17 +0000

    Add new file

---
+++ builds/spawn_build.sh
@@ -0,0 +1,17 @@
+rm -f done.txt
+
+build.sh done.txt &
+
+echo "Waiting for build.sh"
+CNT=0 
+STOPCNT=1200
+SLEEP_SEC=1
+
+while [ $CNT -lt $STOPCNT ]; do
+    echo $CNT
+    if [ -e done.txt ]; then
+        break
+    fi
+    let "CNT = ${CNT} + 1"
+    sleep $SLEEP_SEC
+done
